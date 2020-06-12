6deace9a5d14506e722e69c11415231a4a3ebf1c
liujia02 <bit_liujia@163.com>
2020-06-12 16:33:36 +0800

    chore(build): remove
    remove regenerator

--- config/rollup/rollup.config.js
+++ config/rollup/rollup.config.js
@@ -3,30 +3,74 @@ import path from "path"
 import typescript from "rollup-plugin-typescript2";
 import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
+import SlateVue from '../../packages/slate-vue/package.json'
 
-export default {
-  plugins: [
+const packagesPath = path.join(__dirname, '../../packages')
+
+function configure(pkg, target) {
+  const {name, dependencies} = pkg
+  const rootDir = path.join(packagesPath, name)
+  const isCjs = target === 'cjs'
+  const isEs = target === 'es'
+
+  let babelPresetOptions = {
+    modules: false,
+    useBuiltIns: false,
+    exclude: [
+      '@babel/plugin-transform-regenerator',
+      '@babel/transform-async-to-generator',
+    ],
+  }
+
+  const plugins = [
     typescript({
       exclude: "node_modules/**",
       tsconfig: path.join(__dirname, '../../packages/slate-vue/tsconfig.json'),
     }),
     commonjs(),
     babel({
+      babelHelpers: 'bundled',
       extensions: ['.ts', '.tsx'],
+      presets: [
+        ["@vue/babel-preset-app", babelPresetOptions],
+        ["@vue/babel-preset-jsx", {
+          "injectH": false
+        }]],
     })
-  ],
-  input: path.join(__dirname, '../../packages/slate-vue/index.ts'),
-  output: [{
-    // dir: path.resolve(__dirname, '../../packages/slate-vue/dist'),
-    file: path.resolve(__dirname, '../../packages/slate-vue/dist/index.es.js'),
-    format: "es",
-    sourcemap: true
-  }, {
-    // dir: path.resolve(__dirname, '../../packages/slate-vue/dist'),
-    file: path.resolve(__dirname, '../../packages/slate-vue/dist/index.cjs.js'),
-    format: 'cjs',
-    exports: 'named',
-    sourcemap: true
-  }],
-  external: [ 'slate', 'vue-tsx-support', 'vue-tsx-support/enable-check', 'vue', 'slate-history' ]
-};
+  ]
+  const input = path.join(rootDir, 'index.ts')
+  const external = Object.keys(dependencies).concat(['vue-tsx-support/enable-check', 'vue'])
+
+
+  if(isCjs) {
+    return {
+      plugins,
+      input,
+      external,
+      output: {
+        file: path.resolve(rootDir, `dist/index.${target}.js`),
+        format: target,
+        exports: 'named',
+        sourcemap: true
+      }
+    }
+  }
+
+  if(isEs) {
+    return {
+      plugins,
+      input,
+      external,
+      output: {
+        file: path.resolve(rootDir, `dist/index.${target}.js`),
+        format: target,
+        sourcemap: true
+      }
+    }
+  }
+}
+
+export default [
+  configure(SlateVue, 'es'),
+  configure(SlateVue, 'cjs'),
+]
