baa07f3042b37101acb05357451cc21d30384a39
Oskar Lang <oskar.risto.lang@gmail.com>
2021-10-17 13:29:55 +0200

    fix: correctly implement dragging and dropping

--- packages/slate-vue-shared/components/editable.ts
+++ packages/slate-vue-shared/components/editable.ts
@@ -590,7 +590,7 @@ export const EditableComponent = {
         Transforms.select(editor, range)
       }
 
-      VueEditor.setFragmentData(editor, event.clipboardData)
+      VueEditor.setFragmentData(editor, event.dataTransfer)
     }
   },
   onDrop(event: any, editor: any, ctx: any) {
@@ -599,23 +599,24 @@ export const EditableComponent = {
       !ctx.readOnly &&
       !isEventHandled(event, ctx.onDrop)
     ) {
-      // COMPAT: Certain browsers don't fire `beforeinput` events at all, and
-      // Chromium browsers don't properly fire them for files being
-      // dropped into a `contenteditable`. (2019/11/26)
-      // https://bugs.chromium.org/p/chromium/issues/detail?id=1028668
-      if (
-        !HAS_BEFORE_INPUT_SUPPORT ||
-        (!IS_SAFARI && event.dataTransfer.files.length > 0)
-      ) {
         event.preventDefault()
+
+        const draggedRange = editor.selection
+
         const range = VueEditor.findEventRange(editor, event)
         const data = event.dataTransfer
+        
         Transforms.select(editor, range)
+
+        if (draggedRange) {
+          Transforms.delete(editor, {
+            at: draggedRange,
+          })
+        }
+
         VueEditor.insertData(editor, data)
-      }
     }
   },
-
   onSelectionchange(editor: any, ctx: any) {
     if (!ctx.readOnly && !ctx.isComposing && !ctx.isUpdatingSelection) {
       const { activeElement } = window.document
